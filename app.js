35ffb8e26a893c22b21b70482c63def42f9552e0
Renars <renars.ledins@gmail.com>
2022-12-21 15:24:27 +0200

    enable https

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 3000
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
+const http = require('http');
+const https = require('https');
+
+const privateKey = fs.readFileSync(path.join(__dirname, 'private/privkey.pem'), 'utf8');
+const certificate = fs.readFileSync(path.join(__dirname, 'private/cert.pem'), 'utf8');
+const ca = fs.readFileSync(path.join(__dirname, 'private/chain.pem'), 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'public/model')))
@@ -9,14 +21,18 @@ app.use(express.static(path.join(__dirname, 'public/images')))
 app.use(express.static(path.join(__dirname, 'public/style')))
 app.use(express.static(path.join(__dirname, 'public/javascript')))
 
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/html/index.html'));
 })
 
-app.get('/.well-known/acme-challenge/xnCO8GhMXtTiOGMovFHl9zF3yB_8Mf13qVxPGjsFC4c', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/a.txt'));
-})
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
+
